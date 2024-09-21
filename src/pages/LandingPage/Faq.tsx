25964909f133903bd553f232e32dd6e7441bab69
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-09-21 18:39:10 -0300

    refactor: change faq style to prototype

--- src/pages/LandingPage/Faq.tsx
+++ src/pages/LandingPage/Faq.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { FaPlus, FaMinus } from "react-icons/fa";
 import styled from "styled-components";
 import { theme } from "../../styles/theme";
+import { LuPlus } from "react-icons/lu";
+import { FiMinus } from "react-icons/fi";
 
 const FAQs = [
     { question: "Como crio o anúncio?", answer: "Explicação detalhada de como criar um anúncio." },
@@ -10,62 +11,46 @@ const FAQs = [
     { question: "Vocês garantem que o pet vai ser encontrado?", answer: "Não há garantia, mas maximizamos as chances." },
 ];
 
-const Section = styled.section`
+const SectionWrapper = styled.section`
     display: flex;
-    align-items: center;
+    align-items: flex-start;
     justify-content: space-between;
     flex-wrap: wrap;
-    padding: 0 2rem;
+    padding: 0 7.5rem 0 11rem;
+    gap: 2rem;
     height: auto;
-
-    @media (max-width: 768px) {
-        flex-direction: column;
-        justify-content: center;
-        padding: 3rem 1rem;
-    }
+    width: 95rem;
 `;
 
 const FAQContainer = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 2rem; /* Added more space between FAQ items */
-    padding: 3rem;
-    width: 100%; /* Take full width for better responsiveness */
-    
-    @media (min-width: 768px) {
-        width: 60%; /* Adjust width on larger screens */
-    }
+    gap: 1.375rem;
+    width: 47.5rem;
 `;
 
 const FAQTitle = styled.h2`
     color: ${theme.colors.primary};
     font-weight: bold;
     font-size: 5rem;
+    padding-top: 12.5rem;
 
     > span {
         font-size: 1.5rem;
-        color: ${theme.colors.primary};
-    }
-
-    @media (max-width: 768px) {
-        font-size: 2.5rem; /* Smaller title on mobile */
-        text-align: center;
     }
 `;
 
 const FAQItem = styled.div`
     width: 100%;
-    border-top: 3px solid ${theme.colors.secondary};
-    border-bottom: 3px solid ${theme.colors.secondary};
-    padding: 1rem 0;
+    border-top: 2px solid ${theme.colors.secondary};
+    border-bottom: 2px solid ${theme.colors.secondary};
+    padding: 0.5625rem 0;
     transition: all 0.3s ease-in-out;
-
 
     div {
         display: flex;
-        align-items: right;
-        justify-content: right;
-        padding-right: 1.5rem;
+        align-items: center;
+        justify-content: flex-end;
     }
 `;
 
@@ -74,59 +59,35 @@ const FAQHeader = styled.div`
     align-items: center;
     justify-content: space-between;
     cursor: pointer;
-    
+    padding: 0 3rem;
+
     h3 {
-        padding: 0 2rem;
-        font-size: 1rem;
-        width: 1000px;
+        flex-grow: 1;
     }
 
     div {
-        width: 100px;
+        width: 2rem;
 
         svg {
-            display: flex;
-            font-size: 1.5rem;
-        }
-    }
-
-    @media (max-width: 768px) {
-        h3 {
-            font-size: 1.1rem;
+            font-size: 2rem;
         }
     }
 `;
 
 const FAQAnswer = styled.p`
-    padding: 1rem 3rem 0;
+    padding: 1rem 4rem 0;
     font-size: 1rem;
     color: #444;
     line-height: 1.5;
-
-    @media (max-width: 768px) {
-        padding: 0.5rem 1rem;
-        font-size: 0.9rem;
-    }
 `;
 
 const ImageContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 100%;
+    width: 22rem;
+    margin-top: 2.5rem;
 
     img {
-        max-width: 400px;
-        width: 100%;
+        width: 25rem;
         height: auto;
-        object-fit: cover;
-    }
-
-    @media (max-width: ${theme.breakpoints.tablet}) {
-        display: none;
-    }
-
-    @media (min-width: 768px) {
-        width: 40%;
     }
 `;
 
@@ -138,14 +99,14 @@ const FAQComponent = () => {
     };
 
     return (
-        <Section>
+        <SectionWrapper>
             <FAQContainer>
                 <FAQTitle>FAQ <span>(Perguntas frequentes)</span></FAQTitle>
                 {FAQs.map((faq, index) => (
                     <FAQItem key={index}>
                         <FAQHeader onClick={() => toggleFAQ(index)}>
                             <h3>{faq.question}</h3>
-                            <div>{openFAQ === index ? <FaMinus /> : <FaPlus />}</div>
+                            <div>{openFAQ === index ? <FiMinus /> : <LuPlus />}</div>
                         </FAQHeader>
                         {openFAQ === index && (<FAQAnswer>{faq.answer}</FAQAnswer>)}
                     </FAQItem>
@@ -153,9 +114,9 @@ const FAQComponent = () => {
             </FAQContainer>
 
             <ImageContainer>
-                <img src="https://placehold.co/300x500" alt="Curious cat" />
+                <img src="faq_cat.png" alt="Curious cat" />
             </ImageContainer>
-        </Section>
+        </SectionWrapper>
     );
 };
 
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -5,106 +5,22 @@ import Footer from './Footer';
 import Header from './Header';
 import Hero from './Hero';
 import FAQ from './Faq';
-
+import HowItWorks from './HowItWorks';
+import { theme } from '../../styles/theme';
 
 const PageWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    max-width: 90rem;
+    max-width: 95rem;
     margin: 0 auto;
-    min-height: 100vh;
-`;
-
-const HowItWorks = styled.section`
-    display: flex;
-    color: #f7f7f7;
-    font-weight: 500;
-    padding: 4rem 0 4rem 8rem;
-    width: 100%;
-
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 2rem;
-    height: 400px;
-    background-color: #7821ce;
-    
-    h2 {
-        font-size: 2.5rem;
-        font-weight: bold;
-        color: white;
-    }
-
-    p {
-        text-align: justify;
-        margin: 1rem 0;
-        padding-right: 3.5rem;
-        line-height: 1.5;
-    }
-
-    p:last-of-type {
-        text-align: right;
-        margin-top: 4rem;
-        font-weight: 600;
-    }
+    padding: 2rem 0;
 `;
 
 const HighlightedHistory = styled.section`
-    display: flex;
-    flex-direction: column;
-    padding: 4rem 8rem;
 
-    h2 {
-        font-size: 2.5rem;
-        font-weight: bold;
-        margin-bottom: 2rem;
-        color: #fe5f12;
-    }
-
-    article {
-        display: flex;
-        gap: 1.5rem;
-    }
     
 `;
 
 const HighlightedLostFound = styled.section`
-    display: flex;
-    flex-direction: column;
-    padding: 4rem 8rem;
-    height: 900px;
 
-    h2 {
-        font-size: 2.5rem;
-        font-weight: bold;
-        color: #7821ce;
-    }
-
-    p {
-        font-size: 1.1rem;
-        color: #666666;
-    }
-
-    div {
-        display: flex;
-        gap: 1.5rem;
-    }
-
-    button {
-        background-color: #7821ce;
-        color: white;
-        font-weight: 600;
-        border: none;
-        border-radius: 4px;
-        width: 16%;
-
-        &:hover {
-            background-color: rgba(120, 33, 206, 0.9);
-        }
-
-        padding: 0.75rem 3.5rem;
-        margin: 0 auto;
-        margin-top: 5rem;
-    }
 
 `;
 
@@ -114,14 +30,7 @@ export default function LandingPage() {
             <Header/>
             <main>
                 <Hero/>
-                {/* <HowItWorks>
-                    <div>
-                        <h2>Como funciona o Buscapet</h2>
-                        <p>No Buscapet, cada detalhe conta e cada esforço é valorizado. Estamos aqui para apoiar você, oferecendo uma ferramenta prática e confiável para que você possa se concentrar no que realmente importa: encontrar o seu amigo ou ajudar outro a encontrá-lo.</p>
-                        <p>Obrigado por fazer parte dessa missão conosco. <br/>Juntos, podemos fazer a diferença.</p>
-                    </div>
-                    <img src="https://placehold.co/600x400" alt="About Image" />
-                </HowItWorks> */}
+                <HowItWorks/>
                 {/* <HighlightedHistory>
                     <h2>Histórias em destaque</h2>
                     <article>
