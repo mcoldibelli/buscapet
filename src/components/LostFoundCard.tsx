80108a70e66a1e64d24e3ee1afd027a5fa8d2586
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-09-21 20:11:19 -0300

    refactor: change lost and found style to prototype

--- src/components/LostFoundCard.tsx
+++ src/components/LostFoundCard.tsx
@@ -4,19 +4,20 @@ import styled from 'styled-components';
 const CardContainer = styled.section`
     display: flex;
     flex-direction: column;
-    margin-top: 2.5rem;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
-    width: 350px;
-    border-radius: 8px;
+    height: 31rem;
+    width: 22rem;
+    border-radius: 0.5rem;
+    margin-top: 3rem;
 
     h2 {
         background-color: #7821ce;
+        color: white;
         font-size: 1.5rem;
         font-weight: bold;
-        color: white;
         text-align: center;
-        padding: 1rem;
         border-radius: 0.5rem 0.5rem 0 0;
+        padding: 1.25rem 0;
     }
 `;
 
@@ -24,24 +25,28 @@ const CardContent = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.5rem;
-    padding: 2.5rem;
-    height: 100px;
+    padding: 1.5rem;
+    height: 7.5rem;
 
     div {
         display: flex;
         justify-content: space-between;
         align-items: center;
+        font-size: 1.5rem;
     }
 
     h3 {
-        font-size: 1.7rem;
         font-weight: 600;
-        color: #666666;
+        color: #424242;
     }
 
     svg {
         color: #fe5f12;
-        font-size: 1.5rem;
+    }
+
+    p {
+        font-size: 1rem;
+        color: #424242;
     }
 `;
 
@@ -49,7 +54,7 @@ export default function LostFoundCard() {
     return (
         <CardContainer>
             <h2>Perdido</h2>
-            <img src="https://placehold.co/140x140" alt="avatar" />
+            <img src="https://placehold.co/348x282" alt="avatar" />
             <CardContent>
                 <div>
                     <h3>Nala</h3>
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,27 +1,16 @@
 import styled from 'styled-components';
-import LostFoundCard from '../../components/LostFoundCard';
-import HighlightedCard from '../../components/HighlightedCard';
 import Footer from './Footer';
 import Header from './Header';
 import Hero from './Hero';
 import FAQ from './Faq';
 import HowItWorks from './HowItWorks';
-import { theme } from '../../styles/theme';
+import HighlightedHistory from './HighlightedHistory';
+import HighlightedLostFound from './HighlightedLostFound';
 
 const PageWrapper = styled.div`
     max-width: 95rem;
     margin: 0 auto;
     padding: 2rem 0;
-`;
-
-const HighlightedHistory = styled.section`
-
-    
-`;
-
-const HighlightedLostFound = styled.section`
-
-
 `;
 
 export default function LandingPage() {
@@ -31,24 +20,8 @@ export default function LandingPage() {
             <main>
                 <Hero/>
                 <HowItWorks/>
-                {/* <HighlightedHistory>
-                    <h2>Histórias em destaque</h2>
-                    <article>
-                        <HighlightedCard />
-                        <HighlightedCard />
-                        <HighlightedCard />
-                    </article>
-                </HighlightedHistory>
-                <HighlightedLostFound>
-                    <h2>Achados e perdidos</h2>
-                    <p>Pets anunciados recentemente.</p>
-                    <div>
-                        <LostFoundCard/>
-                        <LostFoundCard/>
-                        <LostFoundCard/>
-                    </div>
-                    <button>Ver mais</button>
-                </HighlightedLostFound> */}
+                <HighlightedHistory/>
+                <HighlightedLostFound/>
                 <FAQ/>
                 <Footer/>
             </main>
