acdff64ccc9eb906d5db1bd82a067da96a88e224
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-09-22 10:43:19 -0300

    feat: add routes container

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import LandingPage from "./pages/LandingPage/LandingPage"
 import './index.css'
 import { ThemeProvider } from "styled-components"
 import { theme } from "./styles/theme"
 import { Analytics } from "@vercel/analytics/react"
+import RouterContainer from "./routes/RouterContainer"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Analytics/>
-      <LandingPage/>
+      <RouterContainer/>
     </ThemeProvider>
   )
 }
