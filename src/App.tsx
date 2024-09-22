3a56fea6ed36cf84e17c87f1c277de7a57570655
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-09-22 10:00:33 -0300

    build: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import LandingPage from "./pages/LandingPage/LandingPage"
 import './index.css'
 import { ThemeProvider } from "styled-components"
 import { theme } from "./styles/theme"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Analytics/>
       <LandingPage/>
     </ThemeProvider>
   )
