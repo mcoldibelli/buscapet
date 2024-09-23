2e1e4f77133bfe6f3b2d741b851071955ecfe516
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-09-23 17:59:02 -0300

    Versioning untracked files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './index.css'
 import { ThemeProvider } from "styled-components"
 import { theme } from "./styles/theme"
-import { Analytics } from "@vercel/analytics/react"
 import RouterContainer from "./routes/RouterContainer"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Analytics/>
       <RouterContainer/>
     </ThemeProvider>
   )
