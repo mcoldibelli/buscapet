3d40a00fac0e631e21237f8979758d18c0c8274d
Henrique Canho <hmcanho@gmail.com>
2024-11-11 18:56:39 -0300

    refactor: group form elements

--- src/pages/Login/SignUp.tsx
+++ src/pages/Login/SignUp.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { theme } from "../../styles/theme";
+import { useNavigate } from "react-router-dom";
 
 const PageWrapper = styled.div`
     display: flex;
@@ -63,28 +64,40 @@ const PageWrapper = styled.div`
 `;
 
 export default function SignUp() {
+    const navigate = useNavigate();
+
+    const handleButtonClick = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); // Previne o comportamento padrão do formulário
+        
+        // Adicione aqui a lógica de envio do formulário, se houver
+        console.log("Formulário enviado com sucesso!");
+
+        // Redireciona para a rota desejada após o envio
+        navigate('/'); // Substitua pela rota de destino
+    };
+
     return (
         <PageWrapper>
             <img src="/register_dog.jpg" alt="Cachorro bege" />
-            <form>
+            <form onSubmit={handleButtonClick}>
                 <h1>Cadastro</h1>
                 <label htmlFor="input-email">
                     Email *
-                    <input type="text" placeholder="E-mail" id="input-email" required/>
+                    <input type="text" placeholder="E-mail" id="input-email" required />
                 </label>
                 <label htmlFor="input-senha">
                     Senha *
-                    <input type="password" placeholder="******" id="input-senha" required/>
+                    <input type="password" placeholder="******" id="input-senha" required />
                 </label>
                 <label htmlFor="input-nome">
                     Nome *
-                    <input type="text" placeholder="Nome" id="input-nome" required/>
+                    <input type="text" placeholder="Nome" id="input-nome" required />
                 </label>
                 <label htmlFor="input-telefone">
                     Telefone
                     <input type="text" placeholder="Telefone" id="input-telefone" />
                 </label>
-                <button>Cadastrar</button>
+                <button type="submit">Cadastrar</button>
             </form>
         </PageWrapper>
     );
