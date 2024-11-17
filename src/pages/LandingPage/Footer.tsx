bb501926e472c4f40444169184b8f914316f75dc
Marcelo Augusto Coldibelli <marcelo.coldibelli@gmail.com>
2024-11-17 17:19:34 -0300

    fix: corrects when searching for a pet

--- src/pages/LandingPage/Footer.tsx
+++ src/pages/LandingPage/Footer.tsx
@@ -1,21 +1,31 @@
 import { IoArrowUpSharp } from "react-icons/io5";
 import { StyledFooter } from "./Footer.style";
+import PetForm from "../../components/forms/PetForm";
+import { useState } from "react";
 
 export default function Footer() {
+    const [isPetFormModalOpen, setIsPetFormModalOpen] = useState(false);
+    const togglePetFormModal = () => {
+        setIsPetFormModalOpen(!isPetFormModalOpen);
+    };
+
     return (
-        <StyledFooter>
-            <div>
-                <h1>buscapet</h1>
-                <a href="/"><IoArrowUpSharp /></a>
-            </div>
+        <>
+            <StyledFooter>
+                <div>
+                    <h1>buscapet</h1>
+                    <a href="/"><IoArrowUpSharp /></a>
+                </div>
 
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/search">Busca</a></li>
-                <li><a href="/publish">Anunciar</a></li>
-            </ul>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/search">Busca</a></li>
+                    <li><a onClick={togglePetFormModal}>Anunciar</a></li>
+                </ul>
 
-            <p>2024 &#169; Buscapet <span>Todos os direitos reservados</span></p>
-        </StyledFooter>
+                <p>2024 &#169; Buscapet <span>Todos os direitos reservados</span></p>
+            </StyledFooter>
+            {isPetFormModalOpen && <PetForm closeModal={togglePetFormModal} />}
+        </>
     )
 }
